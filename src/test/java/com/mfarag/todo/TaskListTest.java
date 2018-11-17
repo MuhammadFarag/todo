b7a032d0f0547e11b9b2faab7c6823782a76c602
Muhammad Farag <muhammad.farag@shopify.com>
2018-11-17 15:54:25 -0500

    #17-CodeRev: Fix code review comments.

--- src/test/java/com/mfarag/todo/TaskListTest.java
+++ src/test/java/com/mfarag/todo/TaskListTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertNull;
 
-public class TodoTest {
+public class TaskListTest {
 
     @Test
     public void TestReturnNoTasksIfNoneExists() {
