97aafc15acf1bc019aa29f8367d43ecf5e9b0fdc
Kareem-Farag <kareemfarag@hotmail.com>
2018-11-18 23:25:58 +0200

    Adding a task for checking if task isn't null

--- src/test/java/com/mfarag/todo/TaskListTest.java
+++ src/test/java/com/mfarag/todo/TaskListTest.java
@@ -2,12 +2,28 @@ package com.mfarag.todo;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class TaskListTest {
 
     @Test
     public void TestReturnNoTasksIfNoneExists() {
+
         assertNull(new TaskList().getTask());
     }
+
+    @Test
+    public void TestReturnTaskIfTaskExists() {
+        TaskList taskList = new TaskList() ;
+        assertNotNull(taskList);
+
+    }
+
+    public Object getTask() {
+        return null;
+    }
+
+
+
 }
