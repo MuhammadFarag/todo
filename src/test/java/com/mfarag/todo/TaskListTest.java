8c3be6d42615b9df99119cd4a3bf5d1d461adfba
Muhammad Farag <muhammad.farag@shopify.com>
2018-11-19 13:34:41 -0500

    #28 drop test coverage

--- src/test/java/com/mfarag/todo/TaskListTest.java
+++ src/test/java/com/mfarag/todo/TaskListTest.java
@@ -2,7 +2,6 @@ package com.mfarag.todo;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class TaskListTest {
@@ -13,13 +12,5 @@ public class TaskListTest {
         assertNull(new TaskList().getTask(0));
     }
 
-    @Test
-    public void TestReturnTaskIfTaskExists() {
-        TaskList taskList = new TaskList();
-        taskList.addTask("New Task");
-        assertNotNull(taskList.getTask(0));
-
-
-    }
 
 }
