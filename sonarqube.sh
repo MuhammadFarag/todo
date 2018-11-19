6cdec0b205a8a36629d0da882e2116f1255e359e
Muhammad Farag <muhammad.farag@shopify.com>
2018-11-19 07:52:53 -0500

    #28: another attempt at sonarqube

--- sonarqube.sh
+++ sonarqube.sh
@@ -9,3 +9,14 @@
   -Dsonar.pullrequest.key=29 \
   -Dsonar.pullrequest.provider=GitHub \
   -Dsonar.pullrequest.github.repository=muhammadfarag/todo
+
+./gradlew build check sonarqube \
+  -Dsonar.host.url=$SONAR_HOST_URL \
+  -Dsonar.login=$SONAR_TOKEN \
+  -Dsonar.analysis.buildNumber=$TRAVIS_BUILD_NUMBER \
+  -Dsonar.analysis.pipeline=$TRAVIS_BUILD_NUMBER \
+  -Dsonar.analysis.sha1=$TRAVIS_PULL_REQUEST_SHA  \
+  -Dsonar.analysis.repository=$TRAVIS_REPO_SLUG \
+  -Dsonar.analysis.prNumber=$TRAVIS_PULL_REQUEST \
+  -Dsonar.branch.name=$TRAVIS_PULL_REQUEST_BRANCH \
+  -Dsonar.branch.target=$TRAVIS_BRANCH
